172198b5e3c9e5e4fe7c98b01897404f60252dd5
Maksudur Rahman <mmaksudurrahman247@gmail.com>
2026-09-12 05:51:29 +0600

    Added hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import HeroBanner from '../assets/banner-stack.png';
 
 function Hero() {
-  return <div className="bg-gray-500 h-screen">Hero</div>;
+  return (
+    <div className="container mx-auto flex gap-48 justify-between items-center py-28">
+      <div className="w-1/2">
+        <h1 className="text-6xl font-bold text-[#0F172A]">
+          Build Your Ideal{' '}
+          <span className="bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent">
+            Development Stack
+          </span>
+        </h1>
+        <p className="text-xl text-[#475569] py-8">
+          Explore frontend, backend, database, and tooling options,
+          <br /> compare them side by side, and put together the stack that fits your next project.
+        </p>
+        <button className="relative w-52 py-3 rounded cursor-pointer text-white mr-4 overflow-hidden bg-linear-to-r from-[#F97316] to-[#EC4899] before:absolute before:inset-0 before:bg-linear-to-r before:from-[#d88549] before:to-[#bb4e85] before:opacity-0 hover:before:opacity-100 before:transition-opacity before:duration-300">
+          <span className="relative z-10">Explore Technologies</span>
+        </button>
+        <button className="border border-[#E5E7EB] hover:bg-[#f3eaea] w-52 py-3 cursor-pointer rounded transition-all duration-300 ease-linear">
+          Learn More
+        </button>
+      </div>
+      <div className="w-1/2">
+        <img className="w-full" src={HeroBanner} alt="Hero Image"></img>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,22 +10,26 @@ function Navbar() {
           <li className="text-[#DB2777]">
             <a href="#">Home</a>
           </li>
-          <li className="hover:text-[#DB2777] transition-colors duration-300 ease-linear">
+          <li className="text-[#475569] hover:text-[#DB2777] transition-colors duration-300 ease-linear">
             <a href="#">Technology</a>
           </li>
-          <li className="hover:text-[#DB2777] transition-colors duration-300 ease-linear">
+          <li className="text-[#475569] hover:text-[#DB2777] transition-colors duration-300 ease-linear">
             <a href="#">Projects</a>
           </li>
-          <li className="hover:text-[#DB2777] transition-colors duration-300 ease-linear">
+          <li className="text-[#475569] hover:text-[#DB2777] transition-colors duration-300 ease-linear">
             <a href="#">About</a>
           </li>
-          <li className="hover:text-[#DB2777] transition-colors duration-300 ease-linear">
+          <li className="text-[#475569] hover:text-[#DB2777] transition-colors duration-300 ease-linear">
             <a href="#">Contact</a>
           </li>
         </ul>
         <div className="flex gap-6">
-          <button>Sign In</button>
-          <button className="bg-[#DB2777] text-white py-3 px-8 rounded-full">Sign Up</button>
+          <button className="cursor-pointer hover:text-[#DB2777] transition-all duration-200 ease-linear">
+            Sign In
+          </button>
+          <button className="bg-[#DB2777] hover:bg-[#a34c73] text-white py-3 px-8 rounded-full cursor-pointer transition-all duration-200 ease-linear">
+            Sign Up
+          </button>
         </div>
       </div>
     </nav>
